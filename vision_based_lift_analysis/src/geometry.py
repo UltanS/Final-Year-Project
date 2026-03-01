74dbcee5c3ebc4bee991b403dc3149d503e1c567
Ultan <ultandoherty04@gmail.com>
2026-03-01 22:49:03 +0000

    Enhance calculate_angle function documentation
    Added detailed docstring to calculate_angle function.

--- vision_based_lift_analysis/src/geometry.py
+++ vision_based_lift_analysis/src/geometry.py
@@ -1,7 +1,30 @@
 import numpy as np
+'''
+    Function: calculate_angle(point_a, vertex_point, point_c
+    Description:
+        This function calculates the angle formed at the vertex
+        point between two connected line segments:
+        point_a -> vertex_point -> point_c
 
+        This is used to measure joint angles such as:
+            Elbow angle - shoulder -> elbow -> wrist
+            Knee angle - hip -> knee -> ankle
+            Hip angle - shoulder -> hip -> knee
+
+        Parameters
+        point_a: tuple(float, float)
+            First point of the line
+        vertex_point: tuple(float, float)
+            Central point of the line where the angle is measured
+        point_c: tuple(float, float)
+            Third point of the line
+
+        Returns a float, the angle at the vertex point
+'''
 def calculate_angle(point_a, vertex_point, point_c):
-  # initialise angle degrees
-  angle_degrees = 0
-
-  return angle_degrees
+    # convert input coordinates to numpy arrays
+    first_point = np.array(point_a)
+    joint_vertex = np.array(vertex_point)
+    third_point = np.array(point_c)
+    
+    return angle_degrees
